738542e45d99c654206917fd1e24b8e8ee8a5fba
Paramesha33 <94032830+Paramesha33@users.noreply.github.com>
2024-09-18 18:23:29 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,44 +1,39 @@
 #include <gtest/gtest.h>
-#include "StringCalculator.h"
- 
-TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
-    int expectedresult = 0;
-    const char* input = "Hello, world!";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
+#include "Soundex.h"
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+//AAA
+  char soundex[5];
+generateSoundex("$rrr", soundex);
+ASSERT_STREQ(soundex,"$666");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
+//AAA
+char soundex[5];
+generateSoundex("pvt", soundex);
+ASSERT_STREQ(soundex,"P130");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
+//AAA
+  char soundex[5];
+generateSoundex("*#12hi", soundex);
+ASSERT_STREQ(soundex,"*000");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_vowels) {
+//AA
+char soundex[5];
+generateSoundex("aeiou", soundex);
+ASSERT_STREQ(soundex,"A000");
 }
  
-TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
-    int expectedresult = 0;
-    const char* input = "0";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_space) {
+//AAA
+char soundex[5];
+generateSoundex(" ", soundex);
+ASSERT_STREQ(soundex," 000");
 }
- 
-TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
-    int expectedresult = 3;
-    const char*  input = "1,2";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
- 
-TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
-    int expectedresult = 6;
-    const char*  input = "1\n2,3";
-    int result =add(input);
-    ASSERT_EQ(result, expectedresult);
-}
- 
-TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
-    int expectedresult = 1;
-    const char*  input = "1,1001";
-    int result =add(input);
-    ASSERT_EQ(result, expectedresult);
-}
- 
-TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
-    int expectedresult = 3;
-    const char*  input = "//;\n1;2";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
+//AAA
+char soundex[5];
+generateSoundex("bcdlmnr", soundex);
+ASSERT_STREQ(soundex,"B234");
 }
